8e913685e6a91527ad0d6f2eeb493d0255db3ed6
Marco Bischoff <bischoff_m@aim.com>
2024-05-23 15:03:41 +0200

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ipa-word-mining
+
+[Data Download Link](https://github.com/open-dict-data/ipa-dict/tree/master/data)
---
+++ sprache.py
@@ -0,0 +1,30 @@
+# %%
+from pathlib import Path
+import pandas as pd
+from IPython.display import display
+import numpy as np
+
+if Path("de.csv").exists():
+    df = pd.read_csv("de.csv")
+else:
+    with open("de.txt", "r", encoding="utf-8") as file:
+        de = file.read().splitlines()
+
+    word, ipa = [], []
+    for line in de:
+        w, i = line.split("\t")
+        for j in i.split(","):
+            word.append(w)
+            ipa.append(j.strip())
+
+    df = pd.DataFrame({"word": word, "ipa": ipa})
+    df.to_csv("de.csv", index=False)
+
+result = df[
+    df["ipa"].str.endswith("ç/")
+    & ~df["word"].str.endswith("ig").astype("bool")
+    & ~df["word"].str.endswith("ich").astype("bool")
+    & ~df["ipa"].str.endswith("ʁç/").astype("bool")
+]
+result.to_csv("query.csv", index=False)
+display(result)
